acffbe42f31e8f45f7157c51f9df8ed9b1b09900
Frank Martinez <frankie.martinez@gmail.com>
2020-05-08 11:48:26 -0700

    🎨 mobile home page adjustment

--- src/components/Landing/About.jsx
+++ src/components/Landing/About.jsx
@@ -6,7 +6,7 @@ const About = () => (
         <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
             What is Magical?
         </h2>
-        <div className="flex items-center flex-wrap mb-20">
+        <div className="flex items-center flex-wrap mb-16">
             <div className="w-full md:w-1/2">
                 <h4 className="text-2xl text-gray-800 font-bold mb-3">RPCD<span className="text-xs" style={{ verticalAlign: "super" }}>&reg;</span></h4>
                 <p className="text-gray-600 mb-8">
@@ -14,13 +14,13 @@ const About = () => (
                     picking and choosing your favorites to add to your own customized collection. 
                 </p>
             </div>
-            <div className="w-full md:w-1/2 flex justify-center">
-                <Keyrune set="ugl"    foil size="6x" className="" rarity="mythic" />
+            <div className="w-full md:w-1/2 flex justify-center" style={{ fontSize: "2rem" }}>
+                <Keyrune set="ugl"    foil size="6x" className="" rarity="uncommon" />
             </div>
         </div>
-        <div className="flex items-center flex-wrap mb-20">
-            <div className="w-full md:w-1/2 flex justify-center">
-                <Keyrune set="iko"    foil size="6x" className="" rarity="mythic" />
+        <div className="flex items-center flex-wrap-reverse md:flex-wrap mb-16">
+            <div className="w-full md:w-1/2 flex justify-center" style={{ fontSize: "2rem" }}>
+                <Keyrune set="iko"    foil size="6x" className="" rarity="rare" />
             </div>
             <div className="w-full md:w-1/2">
                 <h4 className="text-2xl text-gray-800 font-bold mb-3">Search</h4>
@@ -30,14 +30,14 @@ const About = () => (
                 </p>
             </div>
         </div>
-        <div className="flex items-center flex-wrap mb-20">
+        <div className="flex items-center flex-wrap mb-16">
             <div className="w-full md:w-1/2">
                 <h4 className="text-2xl text-gray-800 font-bold mb-3">Collection</h4>
                 <p className="text-gray-600 mb-8">
                     Save your favorites in your virtual collection for later purchase, deck formation, or just because they are so cool. With Magical, you have the power!
                 </p>
             </div>
-            <div className="w-full md:w-1/2 flex justify-center">
+            <div className="w-full md:w-1/2 flex justify-center" style={{ fontSize: "2rem" }}>
                 <Keyrune set="ice"    foil size="6x" className="" rarity="mythic" />
             </div>
         </div>
